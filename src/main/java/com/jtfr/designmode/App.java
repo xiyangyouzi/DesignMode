5f848417bb9c19a531cc67579201f2214f01a98c
chenkangming <chenkangming@xsyxsc.com>
2020-06-19 09:28:07 +0800

    SimpleFactory提交代码

---
+++ src/main/java/com/jtfr/designmode/App.java
@@ -0,0 +1,11 @@
+package com.jtfr.designmode;
+
+/**
+ * Hello world!
+ *
+ */
+public class App {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
---
+++ src/main/java/com/jtfr/designmode/simplefactory/SimpleFactoryMain.java
@@ -0,0 +1,13 @@
+/*
+ * frxs Inc.  湖南兴盛优选电子商务有限公司.
+ * Copyright (c) 2017-2020. All Rights Reserved.
+ */
+
+package com.jtfr.designmode.simplefactory;
+
+public class SimpleFactoryMain {
+
+    public static void main(String[] args) {
+        System.out.println("SimpleFactoryMain");
+    }
+}
